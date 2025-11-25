21f1bc888ed4cbc8d9fd90508055527ff096f270
shpzerra <patipan0646294022@gmail.com>
2025-11-25 15:12:17 +0700

    Enable Home route (/home-profile) in App.jsx

--- smart persona/src/App.jsx
+++ smart persona/src/App.jsx
@@ -3,10 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NoRegister from "./pages/NoRegister";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-// import Dashboard from "./pages/Dashboard";
-// import Home from "./pages/Home";
-// import HomeAdmin from "./pages/HomeAdmin";
-// import HomeHR from "./pages/HomeHR";
+import Home from "./pages/Home";
 import HRLogin from "./pages/HRLogin";
 import HRRegister from "./pages/HRRegister";
 function App() {
@@ -16,9 +13,7 @@ function App() {
         <Route path="/noRegister" element={<NoRegister />} />   
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<RegisterPage />} />
-        {/* <Route path="/home" element={<Dashboard />} /> */}
-        {/* <Route path="/home-profile" element={<Home />} />
-        <Route path="/home-admin" element={<HomeAdmin />} /> */}
+        <Route path="/home-profile" element={<Home />} />
         <Route path="/hrLogin" element={<HRLogin />} />
         <Route path="/hrRegister" element={<HRRegister />} />
         {/* <Route path="/home-hr" element={<HomeHR />} /> */}
